e360e7762b97421dcfffaf8f4af428cc5aa060dd
JQueimado <jpqueimado@gmail.com>
2024-02-21 23:53:06 +0000

    add tables

--- app/TimeSheet.tsx
+++ app/TimeSheet.tsx
@@ -1,7 +1,61 @@
-interface props {
-  name: String;
+import { ReactNode } from "react";
+
+interface lapData {
+  time: String;
+  delta: String;
+  overall: String;
 }
 
-export default async function TimeSheet({ name }: props) {
-  return <div>This is a TimeSheet named {name}</div>;
+interface timeSheetData {
+  date: String;
+  bestLap: String;
+  laps: lapData[];
+}
+
+const data: timeSheetData = {
+  date: "12/03/2024",
+  bestLap: "1.00.000",
+  laps: [
+    { time: "1.00.000", delta: "0", overall: "0" },
+    { time: "2.00.000", delta: "+1.00.000", overall: "+1.00.000" },
+  ],
+};
+
+export default async function TimeSheet() {
+  return (
+    <div className="overflow-x max-w-fit rounded-md bg-base-200">
+      <table className="table">
+        <tbody>
+          <tr>
+            <td>Date:</td>
+            <td> {data.date} </td>
+          </tr>
+          <tr>
+            <td>Best Lap:</td>
+            <td>{data.bestLap}</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="table">
+        <thead>
+          <tr>
+            <td>Lap time</td>
+            <td>Delta</td>
+            <td>Overall</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.laps.map((lap: lapData): ReactNode => {
+            return (
+              <tr>
+                <td>{lap.time}</td>
+                <td>{lap.delta}</td>
+                <td>{lap.overall}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
 import TimeSheet from "./TimeSheet";
 
 export default function Home() {
   return (
     <main>
       <div>Page</div>
-      <TimeSheet name="1" />
-      <TimeSheet name="2" />
-      <TimeSheet name="3" />
+      <div className="grid gap-4 grid-cols-2">
+        <TimeSheet />
+        <TimeSheet />
+        <TimeSheet />
+      </div>
     </main>
   );
 }
